3636f6ac24c0a650413e4644652d85ad2489a572
mAbduqayum <mabduqayum@gmail.com>
2022-04-06 10:44:47 +0500

    updated navigation logic

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,13 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { ExpandingCardsComponent } from '../pages/expanding-cards/expanding-cards.component';
-import { Page404Component } from '../pages/page404/page404.component';
-import { MainComponent } from '../pages/main/main.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import {MaterialExampleModule} from "./material-module";
 import { HeaderComponent } from './header/header.component';
 import { ProjsComponent } from './projs/projs.component';
+import {ExpandingCardsComponent} from "./pages/expanding-cards/expanding-cards.component";
+import {MainComponent} from "./pages/main/main.component";
+import {Page404Component} from "./pages/page404/page404.component";
 
 @NgModule({
   declarations: [
