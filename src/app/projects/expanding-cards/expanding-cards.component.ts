92f77fbd16d74f10aec7cde10699b7fd11a2f92b
mAbduqayum <mabduqayum@gmail.com>
2022-04-07 13:22:46 +0500

    finished expanding-cards

--- src/app/projects/expanding-cards/expanding-cards.component.ts
+++ src/app/projects/expanding-cards/expanding-cards.component.ts
@@ -11,16 +11,31 @@ export class ExpandingCardsComponent implements OnInit {
 
 
   constructor() {
-    for (let i = 1; i < 6; i++) {
-      this.pics.push(`https://picsum.photos/200/600?random=${i}`)
-    }
+    this.initImages();
   }
 
   ngOnInit(): void {
   }
 
   activateImage(index: number): void {
+    if (index === this.activeIndex) {
+      this.activeIndex = -1;
+      return;
+    }
     this.activeIndex = index;
+  }
+
+
+  private initImages() {
+    let width = window.innerWidth - 40 | 0;
+    let height = window.innerHeight - 40 | 0;
+    if (width < 767 + 40) {
+      width = window.innerWidth - 20 | 0;
+      height = window.innerHeight * .8 | 0;
+    }
+    for (let i = 1; i < 6; i++) {
+      this.pics.push(`https://picsum.photos/${width}/${height}?random=${i}`)
+    }
   }
 
 }
